b6435d1aaf62d091aea8cf2d550e53c4b3e17078
amenraprod <amenraprod@gmail.com>
2018-01-07 15:41:10 -0500

    rereouted controller to new view

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/new', function() {
-  return view ('new');
-});
+
+
+Route::get('/new', [
+
+  
+  'uses'=> 'PagesController@new'
+]);
